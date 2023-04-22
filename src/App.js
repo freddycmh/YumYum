119af78ec1a0a59f8ee799be1dd118ff24e4621d
Freddy Mok <fleddymok@gmail.com>
2023-04-21 22:17:45 -0400

    gave the right routes

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ import RootLayout from './Layouts/RootLayout'
 function App() {
   const [festival, useFestival] = useState('queens');
   const [vendors, useVendors] = useState([
-    { name: 'Mcdonalds', location: "brooklyn", type: "Mexican", img: "https://images.pexels.com/photos/1858115/pexels-photo-1858115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
-    { name: 'Wendy', location: "brooklyn", type: "Mexican", img: "https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=1600" },
-    { name: 'Five Guys', location: "brooklyn", type: "Mexican", img: "https://images.pexels.com/photos/4115194/pexels-photo-4115194.jpeg?auto=compress&cs=tinysrgb&w=1600" },
-    { name: 'Prince Tea', location: "brooklyn", type: "Mexican", img: "https://images.pexels.com/photos/16244203/pexels-photo-16244203.jpeg?auto=compress&cs=tinysrgb&w=1600" }
+    { name: 'Mcdonalds', location: "brooklyn", type: "Mexican", route: "/Mcdonalds", img: "https://images.pexels.com/photos/1858115/pexels-photo-1858115.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" },
+    { name: 'Wendy', location: "brooklyn", type: "Mexican", route: "/Wendy", img: "https://images.pexels.com/photos/1633578/pexels-photo-1633578.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+    { name: 'Five Guys', location: "brooklyn", type: "Mexican", route: "/FiveGuys", img: "https://images.pexels.com/photos/4115194/pexels-photo-4115194.jpeg?auto=compress&cs=tinysrgb&w=1600" },
+    { name: 'Prince Tea', location: "brooklyn", type: "Mexican", route: "/PrinceTea", img: "https://images.pexels.com/photos/16244203/pexels-photo-16244203.jpeg?auto=compress&cs=tinysrgb&w=1600" }
   ]);
 
   const router = createBrowserRouter(
@@ -33,7 +33,7 @@ function App() {
           {vendors.map((vendor) => {
 
             return (
-              < Welcome key={uuidv4()} name={vendor.name} type={vendor.type} img={vendor.img} />
+              < Welcome key={uuidv4()} name={vendor.name} type={vendor.type} img={vendor.img} route={vendor.route} />
             )
           })
           }
--- src/components/Home.js
+++ src/components/Home.js
@@ -13,10 +13,8 @@ function Welcome(props) {
                             {props.type}
                         </p>
                     </div>
-                    {/* <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                        <NavLink to='/Mcdonalds'>Home</NavLink>
-                    </button> */}
-                    < NavLink to='/Mcdonalds' >
+
+                    < NavLink to={props.route} >
                         < button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2" >
                             Home
                         </button >
